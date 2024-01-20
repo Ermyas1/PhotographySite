f5464ad1df7b8722677257ef53c2fef510a7588a
Ermyas1 <157008151+Ermyas1@users.noreply.github.com>
2024-01-20 17:52:07 +0300

    add animations

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // const cursor = document.querySelector(".cursor")
-
 // document.addEventListener("mousemove", (e) => {
 //     cursor.style.left = e.pageX + "px";
+//     console.log(e.pageX);
 //     cursor.style.top = e.pageY + 'px';
+//     console.log(e.pageY);
 // })
+
 let text = document.querySelector('.title');
 const leaf = document.getElementById('leaf');
 const hill1 = document.getElementById('hill1');
 const hill4 = document.getElementById('hill4');
 const hill5 = document.getElementById('hill5');
-// let wrapper = document.querySelector('wrapper');
-// console.log(wrapper[0])
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
@@ -20,5 +20,18 @@ window.addEventListener('scroll', () => {
     hill5.style.left = value * 1.5 + "px";
     hill4.style.left = value * -1.5 + "px";
     hill1.style.top = value * 1 + "px";
-
 })
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
